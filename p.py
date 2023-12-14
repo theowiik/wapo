b4928d7040bd44cc03155f1b695a3119f5a102f7
theowiik <42991003+theowiik@users.noreply.github.com>
2023-12-14 22:42:52 +0100

    Add function for return crossword solution

---
+++ p.py
@@ -0,0 +1,27 @@
+import random
+from english_words import get_english_words_set
+
+
+def get_solution(hint: str, word_length: int) -> str:
+    """
+    Get the solution of a word based on a hint and word length.
+    """
+    try:
+        word_length = int(word_length)
+    except:
+        return 'Please provide a number for the word length ✋😒'
+
+    if word_length <= 0:
+        return 'Please provide a positive number ☝️😫'
+
+    word_set = get_english_words_set(['gcide'], lower=True)
+    words_correct_length = [
+        word for word in word_set if len(word) == word_length]
+
+    if not words_correct_length:
+        return 'No words found with that length 😢'
+
+    return f'✅ The word is: "{random.choice(words_correct_length)}" ☝️🤓 ✅'
+
+
+print(get_solution("ww", "0"))
