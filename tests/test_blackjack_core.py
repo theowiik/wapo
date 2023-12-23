493e0ffb500ae97d2a8389a2e469c75624d28219
theowiik <42991003+theowiik@users.noreply.github.com>
2023-12-23 18:08:35 +0100

    Categorize tests

--- tests/test_blackjack_core.py
+++ tests/test_blackjack_core.py
@@ -1,56 +1,57 @@
 import unittest
 from src.blackjack.blackjack_core import Card, Deck, Blackjack, InvalidMove, Suite
 
+class TestCardCreationAndRepresentation(unittest.TestCase):
+    # Tests for Card creation and string representation
 
-class TestCard(unittest.TestCase):
-    def test_error_on_invalid_suit_creation(self):
+    def test_raise_error_for_invalid_suit(self):
         with self.assertRaises(ValueError):
             Card("InvalidSuit", "A")
-
         with self.assertRaises(ValueError):
             Card(None, "A")
-
         with self.assertRaises(ValueError):
             Card("", "A")
 
-    def test_error_on_invalid_rank_creation(self):
+    def test_raise_error_for_invalid_rank(self):
         with self.assertRaises(ValueError):
             Card(Suite.CLUBS, "InvalidRank")
 
-    def test_card_string_representation_format(self):
+    def test_card_string_representation(self):
         card = Card(Suite.HEARTS, "10")
         self.assertEqual(str(card), "10 of Hearts")
 
+class TestDeckOperations(unittest.TestCase):
+    # Tests for Deck operations like drawing and shuffling
 
-class TestDeck(unittest.TestCase):
-    def test_error_when_drawing_from_empty_deck(self):
+    def test_raise_error_when_drawing_from_empty_deck(self):
         deck = Deck()
         deck._cards = []  # Empty the deck
         with self.assertRaises(IndexError):
             deck.draw_card()
 
-    def test_deck_order_changes_after_shuffle(self):
+    def test_deck_is_shuffled_correctly(self):
         deck = Deck()
         before_shuffle = deck._cards.copy()
         deck.shuffle()
         self.assertNotEqual(before_shuffle, deck._cards)
 
+class TestBlackjackGameRules(unittest.TestCase):
+    # Tests for Blackjack game rules and scenarios
 
-class TestBlackjack(unittest.TestCase):
     def setUp(self):
-        self.game = Blackjack()
+        self.game: Blackjack = Blackjack()
 
-    def test_error_when_hitting_after_bust(self):
-        self.game._deal_cards()
+    def test_raise_error_for_hitting_after_bust(self):
+        self.game.deal_cards()
         self.game.player_hand = [
             Card(Suite.HEARTS, "10"),
             Card(Suite.SPADES, "J"),
             Card(Suite.CLUBS, "5"),
         ]
         with self.assertRaises(InvalidMove):
-            self.game._hit_hand(self.game.player_hand)
+            self.game.player_hit()
 
-    def test_bust_scenario_with_aces_in_hand(self):
+    def test_bust_scenario_with_aces(self):
         self.game.player_hand = [
             Card(Suite.HEARTS, "A"),
             Card(Suite.SPADES, "J"),
@@ -71,46 +72,47 @@ class TestBlackjack(unittest.TestCase):
         self.assertEqual(score, 21)
 
     def test_dealer_behavior_on_soft_17(self):
-        # Assuming the game logic makes the dealer hit on soft 17
         self.game.dealer_hand = [Card(Suite.HEARTS, "A"), Card(Suite.SPADES, "6")]
         self.game._dealer_play()
         self.assertGreater(len(self.game.dealer_hand), 2)
 
-    def test_ensure_start_game_before_hit(self):
+class TestBlackjackGameFlow(unittest.TestCase):
+    # Tests for game flow like starting, hitting, standing
+
+    def setUp(self):
+        self.game: Blackjack = Blackjack()
+
+    def test_raise_error_without_starting_game_for_hit(self):
         with self.assertRaises(InvalidMove):
             self.game._hit_hand(self.game.player_hand)
 
-    def test_ensure_start_game_before_stand(self):
+    def test_raise_error_without_starting_game_for_stand(self):
         with self.assertRaises(InvalidMove):
             self.game.player_stand()
 
-    def test_cant_hit_after_stand(self):
+    def test_cant_hit_after_standing(self):
         self.game.deal_cards()
         self.game.player_stand()
         with self.assertRaises(InvalidMove):
             self.game._hit_hand(self.game.player_hand)
 
-    def test_cant_stand_after_stand(self):
+    def test_cant_stand_after_standing(self):
         self.game.deal_cards()
         self.game.player_stand()
         with self.assertRaises(InvalidMove):
             self.game.player_stand()
 
-    def test_cant_deal_cards_after_stand(self):
+    def test_cant_deal_cards_after_standing(self):
         self.game.deal_cards()
         self.game.player_stand()
         with self.assertRaises(InvalidMove):
             self.game._deal_cards()
 
-    def test_cant_deal_cards_after_bust(self):
-        self.game.deal_cards()
-        self.game.player_hand = [
-            Card(Suite.HEARTS, "10"),
-            Card(Suite.SPADES, "J"),
-            Card(Suite.CLUBS, "5"),
-        ]
-        with self.assertRaises(InvalidMove):
-            self.game._deal_cards()
+class TestBlackjackInvalidOperations(unittest.TestCase):
+    # Tests for invalid operations in different game states
+
+    def setUp(self):
+        self.game: Blackjack = Blackjack()
 
     def test_cant_hit_after_bust(self):
         self.game.deal_cards()
@@ -131,6 +133,16 @@ class TestBlackjack(unittest.TestCase):
         ]
         with self.assertRaises(InvalidMove):
             self.game.player_stand()
+
+    def test_cant_deal_cards_after_bust(self):
+        self.game.deal_cards()
+        self.game.player_hand = [
+            Card(Suite.HEARTS, "10"),
+            Card(Suite.SPADES, "J"),
+            Card(Suite.CLUBS, "5"),
+        ]
+        with self.assertRaises(InvalidMove):
+            self.game._deal_cards()
 
     def test_cant_hit_after_blackjack(self):
         self.game.deal_cards()
@@ -156,7 +168,6 @@ class TestBlackjack(unittest.TestCase):
         self.game._dealer_play()
         with self.assertRaises(InvalidMove):
             self.game._hit_hand(self.game.player_hand)
-
 
 if __name__ == "__main__":
     unittest.main()
